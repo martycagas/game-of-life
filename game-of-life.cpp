6c4b2ea1ca89d646fbe70031f61592003e22bf55
Marty Cagas <martycagas@users.noreply.github.com>
2021-04-22 22:55:56 +0200

    Initial commit

---
+++ game-of-life.cpp
@@ -0,0 +1,12 @@
+﻿// game-of-life.cpp : Defines the entry point for the application.
+//
+
+#include "game-of-life.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
