cf1f7f5b591da18893eff58f4a1778088d1d22ea
Marty Cagas <martycagas@users.noreply.github.com>
2021-06-12 22:06:20 +0200

    Moved game's source files under src/

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+/**
+ * @file    main.cpp
+ * @author  Martin Cagas
+ * @date    2021-06-10
+ *
+ * @brief   Conway's Game of Life made using raylib.
+ *
+ * @section DESCRIPTION
+ *
+ * Conway's Game of Life built using C++ and low-level game creation library.
+ *
+ * The main game file. Instatiates the main game class, executes the main loop,
+ * manages game settings and properties.
+ */
+
+#include "main.hpp"
+
+int main()
+{
+    std::cout << "Hello CMake." << std::endl;
+    return 0;
+}
---
+++ src/main.hpp
@@ -0,0 +1,18 @@
+/**
+ * @file    main.hpp
+ * @author  Martin Cagas
+ * @date    2021-06-10
+ *
+ * @brief   Conway's Game of Life made using raylib.
+ *
+ * @section DESCRIPTION
+ *
+ * Conway's Game of Life built using C++ and low-level game creation library.
+ *
+ * The main game file. Instatiates the main game class, executes the main loop,
+ * manages game settings and properties.
+ */
+
+#pragma once
+
+#include <iostream>
